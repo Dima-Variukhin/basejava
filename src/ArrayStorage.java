9848a35e3d24df7ba13fc0dfc45c54e051ec65bf
metamorfag <metamorfag1@gmail.com>
2021-04-02 17:57:35 +0300

    I did first HW

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,74 @@
+
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
+
     }
+
 
     void save(Resume r) {
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+
+        }
     }
+
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            Resume resume = storage[i];
+            if (resume != null && uuid.equals(resume.uuid)) {
+                return resume;
+            } else {
+                break;
+            }
+        }
         return null;
-    }
-
-    void delete(String uuid) {
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
+
+
     }
 
+
     int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                return;
+
+            }
+        }
+        System.out.println("Resume not exist");
     }
 }
