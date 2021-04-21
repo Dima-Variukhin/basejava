9ad22b6d7d507784e6f0954584cf1c3beb9eddae
metamorfag <metamorfag1@gmail.com>
2021-04-21 11:26:05 +0300

    Updated

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -18,37 +18,36 @@ public class ArrayStorage {
         Arrays.fill(storage, 0, size, null);
     }
 
-
     public void update(Resume resume) {
-        try {
-            storage[isResumePresent(resume.getUuid())] = resume;
-            System.out.println("Resume " + storage[isResumePresent(resume.getUuid())].getUuid() + " is updated");
-        } catch (Exception e) {
+        int index = findIndex(resume.getUuid());
+        if (index != -1) {
+            storage[index] = resume;
+            System.out.println("Resume " + storage[index].getUuid() + " is updated");
+        } else {
             System.out.println(resume.getUuid() + R_NOT_PRESENT);
         }
     }
 
-
     public void save(Resume resume) {
-        try {
-            isResumePresent(resume.getUuid());
+        int index = findIndex(resume.getUuid());
+        if (index == -1) {
             if (size < storage.length) {
                 storage[size] = resume;
                 size++;
             } else {
                 System.out.println("List is full");
             }
-        } catch (Exception e) {
+        } else {
             System.out.println(resume.getUuid() + R_PRESENT);
         }
     }
 
     public Resume get(String uuid) {
-        try {
-            return storage[isResumePresent(uuid)];
-        } catch (Exception e) {
-            System.out.println(uuid + R_NOT_PRESENT);
+        int index = findIndex(uuid);
+        if (index != -1) {
+            return storage[index];
         }
+        System.out.println(uuid + R_NOT_PRESENT);
         return null;
     }
 
@@ -64,20 +63,20 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        try {
+        int index = findIndex(uuid);
+        if (index != -1) {
             //мы i-ому элементу присваиваем значение последнего
-            storage[isResumePresent(uuid)] = storage[size - 1];
+            storage[index] = storage[size - 1];
             //а последнего делаем null
             storage[size - 1] = null;
             //уменьшаем размер
             size--;
             return;
-        } catch (Exception e) {
-            System.out.println(uuid + R_NOT_PRESENT);
         }
+        System.out.println(uuid + R_NOT_PRESENT);
     }
 
-    public int isResumePresent(String uuid) {
+    public int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid == storage[i].getUuid()) {
                 return i;
