6f11ee16f0076c4bac47c5e7d24639555774eb4f
metamorfag <metamorfag1@gmail.com>
2021-04-26 14:33:41 +0300

    Updated

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -8,12 +8,10 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     void differ(Resume resume) {
         int index = findIndex(resume.getUuid());
-        if (index < 0) {
             index = -index - 1;
             System.arraycopy(storage, index, storage, index + 1, size - index);
             storage[index] = resume;
             size++;
-        }
     }
 
     @Override
